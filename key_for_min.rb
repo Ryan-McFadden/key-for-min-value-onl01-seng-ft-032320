b8b79bad4972afd78b0f807c575fccedaaf03a69
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-28 03:00:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  low = 0 
+  low_num = 0 
+  low_name = nil
   
+  name_hash.collect do |name, num|
+    
 end
