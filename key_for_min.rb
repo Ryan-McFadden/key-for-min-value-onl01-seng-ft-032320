f6def33d8ec1538db7d4d7ddd19ae4e840d0c655
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-28 02:38:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+#def key_for_min_value(name_hash)
   
-  min = nil 
+ # min = nil 
   
-  name_hash.collect do |name, age|
-    if age < min 
-      min = age
-    end
-  end
+  #name_hash.collect do |name, age|
+  #  if age < min 
+  #    min = age
+  #  end
+ # end
 end
