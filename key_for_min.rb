e4a3b3a849b71db833fa8aaa3f25a368a008141a
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-28 02:17:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,18 +13,3 @@ def key_for_min_value(name_hash)
   end
   small
 end
-
-def key_for_min_value(name_hash)   
-  min_key = nil   
-  min_value = nil   
-  name_hash do |name, age|     
-    if min_value == nil     
-      min_value = age       
-      min_key = name
-    elsif  age < min_value
-       min_value = age       
-      min_key = name
-    end   
-   end   
-   min_key 
- end
